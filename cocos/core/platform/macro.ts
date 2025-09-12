5e063294e970405bec883a50b546e68c353ec4ef
zhefeng.zhang <35944775+zhefengzhang@users.noreply.github.com>
2025-09-12 15:59:57 +0800

    更新 macro.ts

--- cocos/core/platform/macro.ts
+++ cocos/core/platform/macro.ts
@@ -1119,14 +1119,14 @@ interface Macro {
      * WebGL2 & WebGPU 等渲染后端的最大顶点数量通常是 2^32 - 1，即 4294967295。
      * WebGL1 渲染后端的最大顶点数量通常是 2^16 - 1，即 65535。引擎使用拓展 OES_element_index_uint 突破了这个限制，最大顶点数量也可达到 2^32 - 1。
      * 所以 macro.BATCHER2D_MEM_INCREMENT 的理论最大值为 150994943，但是因为所运行平台的 js 引擎对 Float32Array|Uint16Array 的最大长度有限制，实际最大值往往会小于 2^32 - 1。
-     * 建议最大值不超过 2097152 kb (2GB)，否则会因为 js 引擎的限制而导致 buffer 创建失败。
+     * 建议最大值不超过 1048576 kb (1GB)，否则会因为 js 引擎的限制而导致 buffer 创建失败。
      * @en Memory increment (KB) for each rendering batch in Batcher2D
      * Increasing this value can increase the number of 2D rendering components involved in rendering batching, but the memory increment for each additional rendering batch will also become larger.
      * This value determines the number of vertices in each 2D rendering batch, calculated as vCount = Math.floor(macro.BATCHER2D_MEM_INCREMENT * 1024 / 9 * 4);
      * The maximum number of vertices for rendering backends such as WebGL2 & WebGPU is usually 2^32 - 1, which is 4294967295.
      * The maximum number of vertices for the WebGL1 rendering backend is usually 2^16 - 1, which is 65535. The engine uses the extension OES_element_index_uint to break this limit, and the maximum number of vertices can also reach 2^32 - 1.
      * Therefore, the theoretical maximum value of macro.BATCHER2D_MEM_INCREMENT is 150994943. However, due to the limitations of the JS engine on the maximum length of Float32Array|Uint16Array on the running platform, the actual maximum value is often less than 2^32 - 1.
-     * It is recommended that the maximum value does not exceed 2097152 kb (2GB); otherwise, buffer creation will fail due to JS engine limitations.
+     * It is recommended that the maximum value does not exceed 1048576 kb (1GB); otherwise, buffer creation will fail due to JS engine limitations.
      * @example
      * ```typescript
      * import { macro } from 'cc';
